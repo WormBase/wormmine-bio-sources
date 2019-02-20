4979e700a068a31d7cef6962f24b49cc852e3bf3
Paulo Nuin <nuin@genedrift.org>
2019-02-19 19:56:48 -0700

    some files deleted

--- worm-development-ontology/build/gen/org/intermine/model/bio/Intron.java
+++
@@ -1,9 +0,0 @@
-package org.intermine.model.bio;
-
-public interface Intron extends org.intermine.model.bio.SequenceFeature
-{
-    public java.util.Set<org.intermine.model.bio.Gene> getGenes();
-    public void setGenes(final java.util.Set<org.intermine.model.bio.Gene> genes);
-    public void addGenes(final org.intermine.model.bio.Gene arg);
-
-}
